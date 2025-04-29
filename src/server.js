b9f93757a5ccffd87132736a8fa1eab0ca96560f
josemiguel736 <robertz@youremail.com>
2025-04-29 12:21:13 +0200

    fix/merge-error

--- src/server.js
+++ src/server.js
@@ -41,7 +41,8 @@ dotenv.config();
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
 
-
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Conexión a la base de datos
 connectDB();
